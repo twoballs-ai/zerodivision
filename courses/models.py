0f8857e65d7adeb843af84771dd1d9b8db7436d4
twoballs-ai <83840596+twoballs-ai@users.noreply.github.com>
2022-02-28 15:44:06 +0300

    фиксирую конец дня

---
+++ courses/models.py
@@ -0,0 +1,117 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+from .fields import OrderField
+
+# Create your models here.
+
+
+# создание темы курсов
+class Subject(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
+
+
+# Создание модели определяющей курсы в курсах будут модули
+# owner – преподаватель, который создал курс;
+# subjects – предмет, к которому привязан курс. Это внешний ключ, ForeignKey,
+# на модель Subject;
+# title – название курса;
+# slug – слаг курса, мы будем использовать его для формирования челове-
+# копонятных URLʼов;
+# overview – текстовое поле типа TextField для создания краткого описания
+# курса;
+# created – дата и время создания курса, которые будут устанавливаться
+# автоматически, т. к. мы указали auto_now_add=True.
+class Course(models.Model):
+    owner = models.ForeignKey(User,
+                              related_name='courses_created',
+                              on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject,
+                                related_name='courses',
+                                on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    overview = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.title
+
+
+# в курсе может быть несколько модулей
+class Module(models.Model):
+    course = models.ForeignKey(Course,
+                               related_name='modules',
+                               on_delete=models.CASCADE,
+                               limit_choices_to={'model__in': (
+                                   'text',
+                                   'video',
+                                   'image',
+                                   'file')})
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    order = OrderField(blank=True, for_fields=['course'])
+
+    class Meta:
+        ordering = ['order']
+
+    def __str__(self):
+        return '{}. {}'.format(self.order, self.title)
+
+
+class Content(models.Model):
+    module = models.ForeignKey(Module,
+                               related_name='contents',
+                               on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType,
+                                     on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey('content_type', 'object_id')
+    order = OrderField(blank=True, for_fields=['module'])
+
+    class Meta:
+        ordering = ['order']
+
+
+# создание моделей содержимого курса
+class ItemBase(models.Model):
+    owner = models.ForeignKey(User,
+                              related_name='%(class)s_related',
+                              on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.title
+
+
+# добавление абстрактных моделей наследования.
+class Text(ItemBase):
+    content = models.TextField()
+
+
+class File(ItemBase):
+    file = models.FileField(upload_to='files')
+
+
+class Image(ItemBase):
+    file = models.FileField(upload_to='images')
+
+
+class Video(ItemBase):
+    url = models.URLField()
